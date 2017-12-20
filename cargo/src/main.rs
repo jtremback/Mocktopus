318bd396a31f7d456c8e275db4adb6ff549a9234
CodeSandwich <igor.zuk@protonmail.com>
2017-12-20 23:59:27 +0100

    Add copying of dependencies

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -1,14 +1,19 @@
 extern crate cargo;
 extern crate cargo_edit;
+extern crate cargo_metadata;
+extern crate copy_dir;
 extern crate quote;
+extern crate serde_json;
 extern crate syn;
 extern crate toml;
 
 use cargo::core::{Dependency, Manifest, Package, Summary, Workspace};
+use cargo::ops::{self, OutputMetadataOptions};
 use cargo::sources::PathSource;
 use cargo::util::config::Config;
 use cargo::util::important_paths;
 use cargo_edit::{Dependency as DependencyEdit, Manifest as ManifestEdit};
+use cargo_metadata::Metadata;
 use quote::{Tokens, ToTokens};
 use std::fs::{self, File};
 use std::io::{Read, Seek, SeekFrom, Write};
@@ -19,6 +24,36 @@ fn main() {
     let config = Config::default().unwrap();
     let manifest_path = important_paths::find_project_manifest(config.cwd(), "Cargo.toml").unwrap();
     let workspace = Workspace::new(&manifest_path, &config).unwrap();
+
+    let metadata = load_metadata(&workspace);
+//    for package in &metadata.packages {
+//        println!("{:<43} {:<16} {}", package.name, package.version, package.manifest_path);
+//    }
+    let mocktopus_version = metadata.packages.iter()
+        .find(|p| p.name == "mocktopus")
+        .map(|p| p.version.clone());
+
+    let mock_dir = workspace.root().join(".mocktopus");
+    if mock_dir.exists() {
+        fs::remove_dir_all(&mock_dir).unwrap();
+    }
+    fs::create_dir(&mock_dir).unwrap();
+
+    let workspace_name = workspace.current().unwrap().name();
+    for dep_package in &metadata.packages {
+        if dep_package.name == workspace_name {
+            println!("SKIPPING {}", workspace_name);
+            continue;
+        }
+        println!("DEP {} {}", dep_package.name, dep_package.version);
+        let target_dep_dir = mock_dir.join(format!("{} {}", dep_package.name, dep_package.version));
+        let source_dep_dir = Path::new(&dep_package.manifest_path).parent().unwrap();
+        println!("Copy from {:?} to {:?}", source_dep_dir, target_dep_dir);
+        copy_dir::copy_dir(&source_dep_dir, &target_dep_dir).unwrap();
+    }
+
+
+
     let package = workspace.current().unwrap();
     println!("SELF {}", package.name());
     for member in workspace.members() {
@@ -53,7 +88,28 @@ fn main() {
 //    ;
 }
 
+fn load_metadata(workspace: &Workspace) -> Metadata {
+    let output_metadata_ops = OutputMetadataOptions {
+        features: vec![],
+        no_default_features: false,
+        all_features: false,
+        no_deps: false,
+        version: 1,
+    };
+    let export_info = ops::output_metadata(workspace, &output_metadata_ops).unwrap();
+    let metadata_json = serde_json::to_string(&export_info).unwrap();
+    serde_json::from_str(&metadata_json).unwrap()
+}
+
 fn inject_manifest_file<P: AsRef<Path>>(input_path: &Path, output_path: P) {
+    let mut manifest_edit = ManifestEdit::open(&Some(input_path.to_path_buf())).unwrap();
+    let table_path = ["dependencies".to_string()];
+    let mocktopus_dep = DependencyEdit::new("mocktopus").set_version("=0.1.1"); // Get version from root crate deps?
+    manifest_edit.insert_into_table(&table_path, &mocktopus_dep).unwrap();
+    manifest_edit.write_to_file(&mut ::std::fs::File::create(output_path).unwrap()).unwrap();
+}
+
+fn inject_root_manifest_file<P: AsRef<Path>>(input_path: &Path, output_path: P) {
     let mut manifest_edit = ManifestEdit::open(&Some(input_path.to_path_buf())).unwrap();
     let table_path = ["dependencies".to_string()];
     let mocktopus_dep = DependencyEdit::new("mocktopus").set_version("=0.1.1"); // Get version from root crate deps?
---
+++ mtest-test/.mtest/src/another.rs
@@ -0,0 +1,7 @@
+
+extern crate rand as rand4;
+
+pub fn aaa() -> &'static str {
+    rand4::thread_rng();
+    "aaa"
+}
